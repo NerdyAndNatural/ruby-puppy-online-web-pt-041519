f479098443797967cc65f83f49f907c7f875a5d2
Sarah Williams <srwilliams8678@gmail.com>
2019-05-15 01:48:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,20 @@
 class Dog
   attr_accessor :name
   @@all = []
-  
-  def initialize(name)
-    @name = name
-    @age = age
-    @@all << self 
-  end 
-  
-  def self.all
-    @@all
-  end 
-end 
+
+def self.clear_all
+  @@all.clear()
+end
+
+
+def self.all
+@@all.each do |item|
+  puts "#{item.name}"
+end
+end
+
+def initialize(name)
+  @@all.push(self)
+  @name = name
+end
+end
