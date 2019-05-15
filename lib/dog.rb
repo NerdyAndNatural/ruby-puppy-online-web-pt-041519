79a49c36e0bfa1438edc9624fdb54773628a1fdc
Sarah Williams <srwilliams8678@gmail.com>
2019-05-15 01:43:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,29 +1,4 @@
 class Dog
-  attr_accessor :name, :city
-  @@all = []
-
-  def initialize (name, city)
-    @name = name
-    @city = city
-    @dogs = [] 
-    @@all << self 
-  end 
-  
-  def add_dog(dog)
-    dog.shelter = self
-    @dogs << dog 
-  end 
-  
-  def dogs 
-    @dogs 
-  end 
-  
-  def self.all 
-    @@all
-  end 
-end 
-
-class Dog  #belong to a shelter & an owner
   attr_accessor :name, :age, :breed, :shelter, :owner
   @@all = []
   
@@ -38,34 +13,3 @@ class Dog  #belong to a shelter & an owner
     @@all
   end 
 end 
-
-
-class Owner # has many dogs 
-  attr_accessor :name, :age
-  attr_reader :dogs 
-  @@all = []
-  
-   def initialize (name, age)
-    @name = name
-    @age = age
-    @dogs = []
-    @shelters = []
-    @@all << self 
-  end 
-  
-  def add_dog(dog)
-    @dogs << dog 
-    dog.owner = self 
-  end 
-  
-  def shelters
-    self.dogs.each do |dog| 
-      @shelters << dog.shelter if dog.shelter
-    end 
-    @shelters
-  end
-  
-  def self.all 
-    @@all
-  end 
-end
